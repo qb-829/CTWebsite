087d6f0d0add413c2754e7e6c66d5062a7122a74
Quyana Barrow <quyanabarrow@Quyanas-MacBook-Pro.local>
2023-04-23 21:06:05 -0400

    organizing file structure

---
+++ src/Components/Merch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Merch() {
+
+// This page contains listings of Merch items available for purchase
+// -- may outsource to a Shopify store, or similar site.
+
+  return (
+    <div>Merch</div>
+  )
+}
